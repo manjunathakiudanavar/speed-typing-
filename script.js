c0f9ec46f1ca0a8d8e9e9a506d23a6a063cb6085
manjunath k <manjunath.kiudanavar@gmail.com>
2026-06-30 23:40:46 +0530

    features

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
+//  typing area
+ 
  const typingText = document.quesrySelector('.typing-text p');
  const input = document.querySelector(".wrapper .input-field");
+//  statistics
+ const time = document.querySelector(".time span b");
+ const mistakes = document.querySelector(".mistake span");
+ const wpm = document.quesrySelector(".wpm span");
+ const cpm = document.querySelector(".cpm span");
+//  button
+ const btn = document.querySelector(".button")
