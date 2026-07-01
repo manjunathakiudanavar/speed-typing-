337e880970371be7526ea525433ff944b6b1fdeb
manjunath k <manjunath.kiudanavar@gmail.com>
2026-07-01 17:51:11 +0530

    --

--- script.js
+++ script.js
@@ -9,3 +9,18 @@
  const cpm = document.querySelector(".cpm span");
 //  button
  const btn = document.querySelector(".button")
+//  set value
+let timer;
+let timeLeft = maxTime;
+let maxTime= 60;
+let charIndex =0;
+let mistake =0;
+let isTyping = false;
+   
+
+function loadParagraph(){
+    
+const paragraph=[" Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"];
+
+}
+
