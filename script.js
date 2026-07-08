5ba5443b7097a437f570e0492bc5949d5b199ff7
manjunath k <manjunath.kiudanavar@gmail.com>
2026-07-09 03:07:39 +0530

    test js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@
  const input = document.querySelector(".wrapper .input-field");
 //  statistics
  const time = document.querySelector(".time span b");
- const mistakes = document.querySelector(".mistake span");
+ const mistakes = document.querySelector(".mistakes span");
  const wpm = document.querySelector(".wpm span");
  const cpm = document.querySelector(".cpm span");
 //  button
@@ -21,7 +21,7 @@ let isTyping = false;
 
 function loadParagraph(){
     
-const paragraph=[" Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"];
+const paragraph=["Avoid daydreaming about the years to come.","You are the most important person in your whole life.","Always be true to who you are, and ignore what other people have to say about you.","Always be true to who you are, and ignore what other people have to say about you.","Only demonstrate your strength when it’s really required.","Subscribe to Drop X Out"];
 // for getting random paragraphs we use this logic where this logic turns getting the indexes from the paragraph math is an object random() is a function which genrates the random number between 1-5 in decimal math.floor which rounds the number to achivie indexes
 const randomIndex = Math.floor(Math.random()*paragraph.length);
 // inside the HTML tag will be blanked this function is called later once the button clicked try again
@@ -55,8 +55,10 @@ function initTyping(){
             char[charIndex].classList.add('incorrect');
             console.log("incorrect");
         }
-        charcharIndex++;
+        charIndex++;
+        if(charIndex < char.length){
         char[charIndex].classList.add('active');
+        }
 
         mistakes.innerText = mistake;
         cpm.innerText = charIndex-mistake;
@@ -66,5 +68,33 @@ function initTyping(){
         input.value='';
     }
 }
-
-loadParagraph()
+function initTime(){
+    if(timeLeft>0){
+        timeLeft--;
+        time.innerText=timeLeft;
+        let wpmVal = Math.round(((charIndex - mistake)/5)/(maxTime- timeLeft)*60);
+        wpm.innerText = wpmVal;
+    }
+    else{
+        clearInterval(timer);
+    }
+}
+function reset(){
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    time.innerText= timeLeft;
+    input.value='';
+    charIndex = 0;
+    mistake =0;
+    isTyping= false;
+    wpm.innerText=0;
+    cpm.innerText=0;
+    mistakes.innerText=0;
+}
+input.addEventListener("input", (e) => {
+    console.log(e.target.value);
+    initTyping();
+});
+btn.addEventListener("click",reset);
+loadParagraph();
